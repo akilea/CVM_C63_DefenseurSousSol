3c707ed79d27518fb4a45220894c6ccf9037b66f
Marc-André Lavoie <phoenix.lavoie@gmail.com>
2024-08-31 13:59:33 -0400

    Fin de la semaine 01

--- content/chemin_chat.cs
+++ content/chemin_chat.cs
@@ -9,26 +9,31 @@ public partial class chemin_chat : PathFollow2D
     [Export]
     private PackedScene Projectile;
 
+    [Export]
+    private String ActionAvance;
+
+    [Export]
+    private String ActionRecule;
+
+    [Export]
+    private String ActionTire;
+
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
 	{
         float avancement_chat = (float)(delta * VitesseChat);
-        if (Input.IsActionPressed("Chat_Avance"))
+        if (Input.IsActionPressed(ActionAvance))
 		{
 			ProgressRatio += avancement_chat;
         }
-        else if (Input.IsActionPressed("Chat_Recule"))
+        else if (Input.IsActionPressed(ActionRecule))
         {
             ProgressRatio -= avancement_chat;
         }
-        if (Input.IsActionJustPressed("Chat_Tire"))
+        if (Input.IsActionJustPressed(ActionTire))
         {
             BalleVilain projectile = Projectile.Instantiate<BalleVilain>();
-            projectile.LinearVelocity = new Vector2 (1000, 0);
-            //float direction = this.Rotation + Mathf.Pi / 2;
-            //projectile.Rotation = -direction;
-            //projectile.Position = this.Position;
-            //projectile.LinearVelocity = Vector2.Right.Rotated(direction) * projectile.Vitesse;
+            projectile.Position = this.Position;
             this.GetTree().CurrentScene.AddChild(projectile);
         }
     }
