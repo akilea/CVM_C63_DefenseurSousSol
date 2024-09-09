eae88e9d8c487577d516c2ae6bda2a963b99232f
Marc-André Lavoie <phoenix.lavoie@gmail.com>
2024-09-09 11:40:24 -0400

    - Ajout quelques collisions

--- src/Projectile.cs
+++ src/Projectile.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Security.Cryptography;
 
 public partial class Projectile : RigidBody2D
 {
@@ -8,14 +9,19 @@ public partial class Projectile : RigidBody2D
 
 	public override void _Ready()
 	{
-		
+		ContactMonitor = true;
+		MaxContactsReported = 1;
 		LinearVelocity = this.Transform.X * Vitesse;
 	}
 
 	private void OnExitScreen()
 	{
-		GD.PrintT("Bye!");
 		this.QueueFree();
 	}
+
+	private void OnCollision(Node body)
+	{
+        this.GetTree().ReloadCurrentScene();
+    }
 
 }
